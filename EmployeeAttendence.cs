f82f67c5826ca1944dd97c131e11342c5e807530
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-30 18:02:08 +0530

    Cheking Attendence

---
+++ EmployeeAttendence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonStaticEmployeewangProg
+{
+    public class EmployeeAttendence
+    {
+        public void CheckAttendence()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+
+
+        }
+    }
+}
+    
