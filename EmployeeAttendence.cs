b466e323037b8e1e50f6abe9585defc6c028f41e
shwetashriram20 <shwetashriram20@gamil.com>
2022-12-30 18:14:15 +0530

    Switchcase

--- EmployeeAttendence.cs
+++ EmployeeAttendence.cs
@@ -10,14 +10,15 @@ namespace NonStaticEmployeewangProg
     {
         public void CheckAttendence()
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 10;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
 
             if (empCheck == IS_FULL_TIME)
@@ -25,15 +26,21 @@ namespace NonStaticEmployeewangProg
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
             }
-            else
+            else if (empCheck == IS_PART_TIME)
             {
                 Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
 
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
+
             Console.WriteLine("Emp  Wage " + empWage);
         }
     }
 }
-    
+
